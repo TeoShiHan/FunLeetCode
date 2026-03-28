9162988bc5b3b9ca5a901a9741ce2b71a93fa783
Teo Shi Han <teoshihan056@gmail.com>
2026-03-29 03:49:21 +0800

    z

---
+++ IntersectionOfTwoArrays/Solution.cpp
@@ -0,0 +1,110 @@
+/*
+Given two integer arrays nums1 and nums2, return an array of their intersection. 
+Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+ 
+
+Follow up:
+
+What if the given array is already sorted? How would you optimize your algorithm?
+What if nums1's size is small compared to nums2's size? Which algorithm is better?
+What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+
+
+Solutions:
+
+we can do it by search element and replace it with
+-1, then after that the replaced success then record
+it in array.
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+
+        for (int i = 0; i < nums1.size(); i++) {
+            for (int j = 0; j < nums2.size(); j++) {
+                if (nums1[i] == nums2[j]) {
+                    result.push_back(nums1[i]);
+                    nums2[j] = -1;   // mark as used
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+// but seems too slow?
+
+also we maybe can like ...
+calculate the frequency? in sorted map,
+
+
+// so far this is best solution from result
+
+how he think:
+
+We sort both arrays first, so same values will be grouped together.
+Then we use two pointers to scan both arrays at the same time.
+
+
+if same then add to array,
+
+if not same, traverse the smaller one to one more step
+
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
+    {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        vector<int> result;
+
+        for (int i = 0,  j = 0; i < nums1.size() && j < nums2.size();){
+            if (nums1[i] > nums2[j]){
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                result.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        return result;
+
+    }
+}; 
+
+
+eg: 1,1,2,3,
+eg: 1,2,3
+
+
+T1 T2 T3 t4
+1  1  2  3
+1  2  2  3
+
+
+*/
